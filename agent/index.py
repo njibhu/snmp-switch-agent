a444dbb951a9cbf27ad6dae222169bc566291571
Njibhu <manu@njibhu.eu>
2020-10-05 22:43:46 +0200

    Fix update method

--- agent/index.py
+++ agent/index.py
@@ -12,22 +12,22 @@ class IfSwitchPorts(pyagentx.Updater):
         self.set_INTEGER('1', amount)
         for port_index in range(amount):
             port_detail = swconfig.get_port_detail(port_index)
-            self.set_INTEGER('2.' + port_index + '.1', port_index)
+            self.set_INTEGER('2.' + str(port_index) + '.1', port_index)
             port_up = 1 if port_detail["link"]["link"] == 'up' else 2
-            self.set_INTEGER('2.' + port_index + '.2', port_up)
+            self.set_INTEGER('2.' + str(port_index) + '.2', port_up)
             port_speed = 0
             if "speed" in port_detail["link"]:
                 match_number = re.match(r"\d*", port_detail["link"]["speed"])
                 port_speed = int(match_number.group())
-            self.set_GAUGE32('2.' + port_index + '.3', port_speed)
+            self.set_GAUGE32('2.' + str(port_index) + '.3', port_speed)
             rx_good_bytes = 0
             if "RxGoodByte" in port_detail["mib"]:
                 rx_good_bytes = port_detail["mib"]["RxGoodByte"]
-            self.set_COUNTER64('2.' + port_index + '.4', rx_good_bytes)
+            self.set_COUNTER64('2.' + str(port_index) + '.4', rx_good_bytes)
             tx_bytes = 0
             if "TxByte" in port_detail["mib"]:
                 tx_bytes = port_detail["mib"]["TxByte"]
-            self.set_COUNTER64('2.' + port_index + '.5', tx_bytes)
+            self.set_COUNTER64('2.' + str(port_index) + '.5', tx_bytes)
 
 
 class MyAgent(pyagentx.Agent):
